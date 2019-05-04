ee6d2475c5617b2ab6ad7d96a090cad28898ae55
Doan Hong Ngoc <ngocdh236@gmail.com>
2019-05-04 15:42:26 +0300

    Initial Commit

---
+++ Flashcard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcard
+//
+//  Created by Hồng Ngọc Doãn on 5/4/19.
+//  Copyright © 2019 Titek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
